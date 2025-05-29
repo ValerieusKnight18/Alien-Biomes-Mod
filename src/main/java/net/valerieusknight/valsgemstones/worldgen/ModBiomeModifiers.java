ab9a6aa91a6b7b7570c69eff34b49577a418b76e
ValerieusKnight18 <priscoohno@gmail.com>
2025-05-29 11:48:15 -0400

    Ore Generation

--- src/main/java/net/valerieusknight/valsgemstones/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/valerieusknight/valsgemstones/worldgen/ModBiomeModifiers.java
@@ -1,18 +1,28 @@
 package net.valerieusknight.valsgemstones.worldgen;
 
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import net.valerieusknight.valsgemstones.ValsGemstones;
 
 public class ModBiomeModifiers {
 
+    public static final ResourceKey<BiomeModifier> ADD_SAPPHIRE_ORE = registerKey("add_sapphire_ore");
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
+
+        context.register(ADD_SAPPHIRE_ORE, new BiomeModifiers.AddFeaturesBiomeModifier(
+            biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+            HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SAPPHIRE_ORE_PLACED_KEY)),
+            GenerationStep.Decoration.UNDERGROUND_ORES));
     }
 
     public static ResourceKey<BiomeModifier> registerKey(String name) {
